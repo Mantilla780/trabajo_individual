47aa8016297f7fdd6991cdf24c1e4691f757651c
Manta780 <133817781+Manta780@users.noreply.github.com>
2024-10-25 15:26:50 -0500

    Commit
    s

--- Proyecto_Inovahouse/src/main/java/Modelo/Deuda.java
+++ Proyecto_Inovahouse/src/main/java/Modelo/Deuda.java
@@ -4,10 +4,38 @@
  */
 package Modelo;
 
-/**
- *
- * @author USER
- */
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Deuda {
+    @Id
+    private String ID_Deuda;
+    private String Tipo_Deuda;
+
+    public Deuda() {
+    }
+
+    public Deuda(String ID_Deuda, String Tipo_Deuda) {
+        this.ID_Deuda = ID_Deuda;
+        this.Tipo_Deuda = Tipo_Deuda;
+    }
+
+    public String getID_Deuda() {
+        return ID_Deuda;
+    }
+
+    public void setID_Deuda(String ID_Deuda) {
+        this.ID_Deuda = ID_Deuda;
+    }
+
+    public String getTipo_Deuda() {
+        return Tipo_Deuda;
+    }
+
+    public void setTipo_Deuda(String Tipo_Deuda) {
+        this.Tipo_Deuda = Tipo_Deuda;
+    }
+    
     
 }
--- Proyecto_Inovahouse/src/main/java/Modelo/Usuario.java
+++ Proyecto_Inovahouse/src/main/java/Modelo/Usuario.java
@@ -4,10 +4,68 @@
  */
 package Modelo;
 
-/**
- *
- * @author USER
- */
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+    @Id
+    private String ID_Usuario;
+    private String Nombre_Usuario;
+    private int Telefono_Usuario;
+    private String Correo_Usuario;
+    private String Tipo_Usuario;
+
+    public Usuario() {
+    }
+
+    public Usuario(String ID_Usuario, String Nombre_Usuario, int Telefono_Usuario, String Correo_Usuario, String Tipo_Usuario) {
+        this.ID_Usuario = ID_Usuario;
+        this.Nombre_Usuario = Nombre_Usuario;
+        this.Telefono_Usuario = Telefono_Usuario;
+        this.Correo_Usuario = Correo_Usuario;
+        this.Tipo_Usuario = Tipo_Usuario;
+    }
+
+    public String getID_Usuario() {
+        return ID_Usuario;
+    }
+
+    public void setID_Usuario(String ID_Usuario) {
+        this.ID_Usuario = ID_Usuario;
+    }
+
+    public String getNombre_Usuario() {
+        return Nombre_Usuario;
+    }
+
+    public void setNombre_Usuario(String Nombre_Usuario) {
+        this.Nombre_Usuario = Nombre_Usuario;
+    }
+
+    public int getTelefono_Usuario() {
+        return Telefono_Usuario;
+    }
+
+    public void setTelefono_Usuario(int Telefono_Usuario) {
+        this.Telefono_Usuario = Telefono_Usuario;
+    }
+
+    public String getCorreo_Usuario() {
+        return Correo_Usuario;
+    }
+
+    public void setCorreo_Usuario(String Correo_Usuario) {
+        this.Correo_Usuario = Correo_Usuario;
+    }
+
+    public String getTipo_Usuario() {
+        return Tipo_Usuario;
+    }
+
+    public void setTipo_Usuario(String Tipo_Usuario) {
+        this.Tipo_Usuario = Tipo_Usuario;
+    }
+    
     
 }
