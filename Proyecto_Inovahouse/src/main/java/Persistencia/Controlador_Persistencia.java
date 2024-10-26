7c8ff7e0c167db7176f4e8e0cafe13a99ebd8c99
Manta780 <133817781+Manta780@users.noreply.github.com>
2024-10-25 21:27:54 -0500

    puta mierda

--- Proyecto_Inovahouse/src/main/java/Persistencia/Controlador_Persistencia.java
+++ Proyecto_Inovahouse/src/main/java/Persistencia/Controlador_Persistencia.java
@@ -4,10 +4,28 @@
  */
 package Persistencia;
 
+import Modelo.ProyectoVivienda;
+
 /**
  *
  * @author Admin
  */
 public class Controlador_Persistencia {
-    
+    ApartamentoJpaController apartamentoJpa = new ApartamentoJpaController();
+    ClienteJpaController clienteJpa = new ClienteJpaController();
+    DeudaJpaController deudaJpa = new DeudaJpaController();
+    PagoJpaController pagoJpa = new PagoJpaController();
+    ProyectoViviendaJpaController proyectoviviendaJpa = new ProyectoViviendaJpaController();
+    TorreJpaController torreJpa = new TorreJpaController();
+    UsuarioJpaController usuarioJpa = new UsuarioJpaController();
+    VentaJpaController ventaJpa = new VentaJpaController();
+
+    public void guardarProyectoVivienda(ProyectoVivienda proyecto) throws Exception {
+    try {
+        proyectoviviendaJpa.create(proyecto);
+    } catch (Exception e) {
+        e.printStackTrace(); // Esto es solo para propósitos de depuración
+        throw e; // Vuelve a lanzar la excepción
+        }
+    }
 }
