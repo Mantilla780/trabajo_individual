93587838d90097dadc9ece3ec9122fb63c690677
Mistogo2 <omacias1@udi.edu.co>
2024-10-27 19:17:17 -0500

    Add files via upload

---
+++ innovahouse/src/main/java/Controlador/ConexionBD.java
@@ -0,0 +1,52 @@
+package Controlador;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class ConexionBD {
+    private static ConexionBD instancia;
+    private Connection con = null;
+    private String url = "jdbc:oracle:thin:@localhost:1521:XE";
+    private String user = "proyecto";
+    private String password = "proyecto";
+
+    public ConexionBD() {
+        try {
+            con = DriverManager.getConnection(url, user, password);
+            if (con != null) {
+                DatabaseMetaData meta = con.getMetaData();
+                System.out.println("Base de datos conectada: " + meta.getDriverName());
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error de conexión: " + ex.getMessage());
+        }
+    }
+
+    public static ConexionBD getInstancia() {
+        if (instancia == null) {
+            instancia = new ConexionBD();
+        }
+        return instancia;
+    }
+
+    public Connection getConnection() {
+        return con;
+    }
+
+    public void closeConnection() {
+        if (con != null) {
+            try {
+                con.close();
+                System.out.println("Conexión cerrada.");
+            } catch (SQLException ex) {
+                Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
+
